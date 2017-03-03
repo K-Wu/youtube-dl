7f979ac70ab4cf6dbaa861ae72e35f476dc5ce2f
K-Wu <tonywukun@qq.com>
2017-03-03 08:55:23 +0800

    Update wine-py2exe.sh

--- wine-py2exe.sh
+++ wine-py2exe.sh
@@ -23,9 +23,7 @@ if [ ! -d wine-py2exe ]; then
     # http://appdb.winehq.org/objectManager.php?sClass=version&iId=21957
     echo "Follow python setup on screen"
     wine msiexec /i python-3.3.5.msi
-    cd py2exe-3
-    wine "C:\\Python33\\python.exe" setup.py install
-    cd ..
+    wine "C:\\Python33\\python.exe\\Scripts\\pip.exe"  pyinstaller
     echo "Follow py2exe setup on screen"
     
     
@@ -51,7 +49,7 @@ else
 
 fi
 
-wine "C:\\Python33\\python.exe" "$1" py2exe > "py2exe.log" 2>&1 || true
+wine"C:\\Python33\\Scripts\\pyinstaller.exe" --onefile "$1" > "py2exe.log" 2>&1 || true
 #echo '# Copying python27.dll' >> "py2exe.log"
 #cp "$WINEPREFIX/drive_c/windows/system32/python27.dll" build/bdist.win32/winexe/bundle-2.7/
-wine "C:\\Python33\\python.exe" "$1" py2exe >> "py2exe.log" 2>&1
+#wine "C:\\Python33\\python.exe" "$1" py2exe >> "py2exe.log" 2>&1
